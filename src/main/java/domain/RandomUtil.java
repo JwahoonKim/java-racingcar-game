1c6f2ce036502df26d6454c4c9931e0556d5e80a
jwahoonKim <kjhoon0330@snu.ac.kr>
2022-05-24 02:29:30 +0900

    feat: 0 ~ 9 난수 발생을 위한 RandomUtil Class 구현

---
+++ src/main/java/domain/RandomUtil.java
@@ -0,0 +1,11 @@
+package domain;
+
+import java.util.Random;
+
+public class RandomUtil {
+    static Random random = new Random();
+
+    public static int getRandomNumber() {
+        return random.nextInt(10);
+    }
+}
