0c58f722b34894dd90aacc44063e21259abc0a36
jwahoonKim <kjhoon0330@snu.ac.kr>
2022-05-27 16:11:21 +0900

    test: CarTest 수정

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,4 +1,5 @@
 import domain.Car;
+import domain.Position;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,12 +15,12 @@ public class CarTest {
     @Test
     void 숫자가_4보다_크면_앞으로_간다() {
         car.race(4);
-        Assertions.assertThat(car.getDistance()).isEqualTo(1);
+        Assertions.assertThat(car.getPosition()).isEqualTo(new Position(1));
     }
 
     @Test
     void 숫자가_4보다_작으면_안간다() {
         car.race(1);
-        Assertions.assertThat(car.getDistance()).isEqualTo(0);
+        Assertions.assertThat(car.getPosition()).isEqualTo(new Position(0));
     }
 }
