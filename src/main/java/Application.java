ff2fa34b5c94aec5f11e457dffcedb6ca91963ee
jwahoonKim <kjhoon0330@snu.ac.kr>
2022-05-27 16:13:05 +0900

    refactor: 의존 객체 변경에 따른 가벼운 수정

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,8 @@
-import domain.Race;
+import domain.RacingGame;
 
 public class Application {
     public static void main(String[] args) throws Exception {
-        Race race = new Race();
-        race.play();
+        RacingGame racingGame = new RacingGame();
+        racingGame.play();
     }
 }
--- src/main/java/view/ResultView.java
+++ src/main/java/view/ResultView.java
@@ -1,13 +1,13 @@
 package view;
 
 import domain.Car;
-import domain.Stadium;
+import domain.Cars;
 
 import java.util.List;
 
 public class ResultView {
     public static void printStatus(List<Car> cars) {
-        cars.forEach(car -> System.out.println(car.getName() + " : " + "-".repeat(car.getDistance())));
+        cars.forEach(car -> System.out.println(car.getName() + " : " + "-".repeat(car.getPosition().getPosition())));
         System.out.println();
     }
 
@@ -16,8 +16,8 @@ public class ResultView {
     }
 
 
-    public static void printResult(Stadium st) throws Exception {
-        List<Car> winner = st.getWinner();
+    public static void printResult(Cars cars) throws Exception {
+        List<Car> winner = cars.getWinners();
         winner.forEach(car -> System.out.print(car.getName() + ", "));
         System.out.println("가 최종 우승했습니다.");
     }
